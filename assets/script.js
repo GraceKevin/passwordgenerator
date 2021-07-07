01987a83583ac79aac219648ad7bf708c9ec8335
Kevin Grace <Kevin@kevins-air.home>
2021-07-06 20:52:58 -0400

    refurb const to var and added if statements

--- assets/script.js
+++ assets/script.js
@@ -7,69 +7,65 @@ document.querySelector("#generate").addEventListener("click", writePassword);
 
 // setup constants in place of variables 
 
-const lengthElement = document.getElementById('length');
-const uppercaseElement = document.getElementById('uppercase');
-const lowercaseElement = document.getElementById('lowercase');
-const numbersElement = document.getElementById('numbers');
-const symbolsElement = document.getElementById('symbols');
-const generateBtn = document.getElementById('generate');
+var lengthElement = document.getElementById('length');
+var uppercaseElement = document.getElementById('uppercase');
+var lowercaseElement = document.getElementById('lowercase');
+var numbersElement = document.getElementById('numbers');
+var symbolsElement = document.getElementById('symbols');
+var generateBtn = document.getElementById('generate');
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-}
+var symbols = "!@#$%^&*()";
+var numbers = "1234567890";
+var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercases = "abcdefghijklmnopqrstuvwxyz";
+
 
 // Prompt to confirm how many characters the user would like in their password
-function generatePassword(lower, upper, number, symbols, length) {
-  const lengthElement = (prompt("Please enter the length of the password from 8 to 128"));
-
+function generatePassword() {
+  var lengthElement = (prompt("Please enter the length of the password from 8 to 128"));
+ //console.log (generatePassword);
+ 
   // Loop if answer is outside the parameters 
   while(lengthElement <= 7 || lengthElement >= 128) {
       alert("Password length must be between 8 - 128 characters Try again");
-      const lengthElement = (prompt("How many characters would you like your password to contain?"));
-      } 
+      var lengthElement = (prompt("How many characters would you like your password to contain?"));
+      }
 
       // Repeat back how many charactes the user will have  
       alert(`Your password will have ${lengthElement} characters`);
-
+      var passwordCharacters = []
+      var randomPassword = ""
     // Determine parameters of password 
     const symbolsElement = confirm("Click OK to confirm if you would like to include symbols");
+    if (symbolsElement) {
+      passwordCharacters.push (symbols)
+    }
     const numbersElement = confirm("Click OK to confirm if you would like to include numbers");    
+    if (numbersElement) {
+      passwordCharacters.push(numbers)
+    }
     const lowercaseElement = confirm("Click OK to confirm if you would like to include lowercase letters");
+    if (lowercaseElement) {
+      passwordCharacters.push(lowercases)
+    }
     const uppercaseElement = confirm("Click OK to confirm if you would like to include uppercase letters");
+    if (uppercaseElement) {
+      passwordCharacters.push(uppercases)
+    }
+    passwordCharacters = passwordCharacters.join("");
+
       // Loop if answer is outside the parameters 
       while (symbolsElement === false && numbersElement === false && lowercaseElement === false && uppercaseElement === false)
       alert("Please select whether you would like to include the parameter");
       
       // Assign an action to the password parameters
-      var passwordCharacters = [number + symbols + upper + lower]
-      
-    if (symbolsElement) {
-      passwordCharacters = passwordCharacters.concat(symbols)
-    }
-
-    if (numbersElement) {
-      passwordCharacters = passwordCharacters.concat(number)
-    }
-      
-    if (lowercaseElement) {
-      passwordCharacters = passwordCharacters.concat(lower)
-    }
-
-    if (uppercaseElement) {
-      passwordCharacters = passwordCharacters.concat(upper)
-    }
-}
+      for (var i = 0; i < lengthElement; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+        }
+        return randomPassword;
+      }
 // Empty string to be filled based on for loop selecting random characters from the array
-function randomPassword(){
-  for (var i = 0; i < lengthElement; i++) {
-  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-  console.log(randomPassword)
-  }
-  return randomPassword;
-}
 
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -91,7 +87,9 @@ function getRandomSymbol() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log (password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  //console.log ('#password');
 }
